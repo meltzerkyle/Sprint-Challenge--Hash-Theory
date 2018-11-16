a505e7b5b96d40f05f11eaa2bdbcce01caeeeaba
meltzerkyle <meltzerkyle@gmail.com>
2018-11-16 12:33:25 -0600

    completed ex1

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -8,15 +8,52 @@ Answer *get_indices_of_item_weights(int *weights, int length, int limit)
   HashTable *ht = create_hash_table(16);
 
   // YOUR CODE HERE
+  Answer *answer = malloc(sizeof(Answer));
+
+  for (int i = 0; i < length; i++)
+  {
+    hash_table_insert(ht, weights[i], i);
+    // printf("%i\n", hash_table_retrieve(ht, weights[i]));
+  }
+
+  for (int i = 0; i < length; i++)
+  {
+    int temp = hash_table_retrieve(ht, limit - weights[i]);
+    if (temp != -1)
+    {
+      answer->index_1 = temp;
+      answer->index_2 = i;
+      return answer;
+    }
+  }
 
   return NULL;
 }
 
 void print_answer(Answer *answer)
 {
-  if (answer != NULL) {
+  if (answer != NULL)
+  {
     printf("%d %d\n", answer->index_1, answer->index_2);
-  } else {
+  }
+  else
+  {
     printf("NULL\n");
   }
 }
+
+#ifndef TESTING
+int main(void)
+{
+  int weights_4[] = {12, 6, 7, 14, 19, 3, 0, 25, 40};
+
+  Answer *answer_4 = get_indices_of_item_weights(weights_4, 9, 7);
+
+  if (answer_4->index_1 != 6)
+    printf("Your function did not return the expected answer for input {12, 6, 7, 14, 19, 3, 0, 25, 40}");
+  if (answer_4->index_2 != 2)
+    printf("Your function did not return the expected answer for input {12, 6, 7, 14, 19, 3, 0, 25, 40}");
+
+  return 0;
+}
+#endif
